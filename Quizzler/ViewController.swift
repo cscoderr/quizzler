14f52b8e2e2121222990bd3082f4c46c6cb1c3ca
Christopher Idowu <tommydprogrammer@gmail.com>
2022-07-17 03:09:21 +0100

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Tomiwa Idowu on 17/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
